48125d5015ed0bd5d829bbf9504941bd389916e2
northg <gillyspy@gmail.com>
2020-10-02 21:12:23 -0400

    css abstractions

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const postcssplugins = [
+    require('postcss-import'),
     require('postcss-simple-vars'),
     require('postcss-nested'),
     require('autoprefixer'),
