3c8b6cc37261c84d8cce009fdce3d4df2d5ea75f
northg <gillyspy@gmail.com>
2020-10-04 22:55:34 -0400

    cool transitions for mobile menu

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,17 @@
 import '../styles/styles.css';
+import MobileMenu from './modules/MobileMenu.js';
+
+
+let mobileMenu = new MobileMenu(
+    '.site-header--menu-icon',
+    '.site-header--menu-content',
+    'site-header--menu-content--is-visible',
+    '.site-header',
+    'site-header--is-expanded',
+    'site-header--menu-icon--close-x'
+);
 
 if(module.hot){
     module.hot.accept();
 }
+
