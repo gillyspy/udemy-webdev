4e82404f9d2a22f719e35d40fa13160dc5633eae
northg <gillyspy@gmail.com>
2020-10-08 18:12:04 -0400

    delay loading of script and html for modal until requested

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,7 +2,7 @@ import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu.js'
 import ScrollDetect from './modules/ScrollDetect.js'
 import LockHeader from './modules/LockHeader.js';
-import Modal from './modules/Modal.js';
+
 
 function docReady(fn) {
     // see if DOM is already available
@@ -15,13 +15,8 @@ function docReady(fn) {
     }
 }
 
-
 docReady(function() {
     // DOM is loaded and ready for manipulation here
-
-    window.GMoney = new Modal('contactModal', '.modalTrigger', 'modal--top', 'modal--bottom')
-
-
     new LockHeader('.site-header', 'site-header--dark',
         {
             sectionSelector: '.page-section',
@@ -39,6 +34,27 @@ docReady(function() {
         'site-header--is-expanded',
         'site-header--menu-icon--close-x'
     );
+    let modal;
+    document.querySelectorAll( '.modalTrigger' ).forEach( el => {
+        el.addEventListener( 'click', ev => {
+            ev.preventDefault()
+            let t = 1000;
+            if( typeof modal === 'undefined' ) { //load if needed
+                import(/* webpackChunkName: "modal" */ './modules/Modal.js').then( x => {
+                    modal = new x.default('contactModal', '.modalTrigger', 'modal--top', 'modal--bottom')
+                    setTimeout( ()=> {
+                        modal.openModal();
+                        //trigger modal 1st time
+                    },t)
+                }).catch((e) => {
+                    console.log('there was a problem loading the JS for modal', e);
+                })
+            } else {
+                modal.openModal();
+            }
+
+        })
+    })
 
     if (module.hot) {
         module.hot.accept();
--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -6,36 +6,57 @@ class Modal {
                 escapeKeys
     ) {
         this.escapeKeys =  escapeKeys || [27]
-        this.modal = document.getElementById(idForShow)
+        this.modal
         this.classForShow = classForShow
         this.classForHide = classForHide
+        this.idForShow = idForShow
         this.selectorTrigger = selectorTrigger
+        this.openIt = this.openModal
         this.init()
         //nothing yet
     }
     init(){
-        this.toggleSelector(this.classForHide)
+        this.insertHtml()
+        this.modal = document.getElementById(this.idForShow)
+        //this.toggleSelector(this.classForShow)
         this.beginEvents()
     }
 
+    insertHtml(){
+        document.body.insertAdjacentHTML('beforeend',
+            `<div id="contactModal" class="modal">
+  <div class="modal__inner">
+    <h2 class="section-title section-title--blue section-title--less-margin"><img src="assets/images/icons/mail.svg" class="section-title__icon"> Get in <strong>Touch</strong></h2>\
+    <div class="wrapper wrapper--narrow">
+      <p class="modal__description">We will have an online order system in place soon. Until then, connect with us on any of the platforms below!</p>
+    </div>
+    <div class="social-icons">
+      <a href="#" class="social-icons__icon"><img src="assets/images/icons/facebook.svg" alt="Facebook"></a>
+      <a href="#" class="social-icons__icon"><img src="assets/images/icons/twitter.svg" alt="Twitter"></a>
+      <a href="#" class="social-icons__icon"><img src="assets/images/icons/instagram.svg" alt="Instagram"></a>
+      <a href="#" class="social-icons__icon"><img src="assets/images/icons/youtube.svg" alt="YouTube"></a>
+    </div>
+  </div>
+  <div id="modalClose" class="modal__close modalTrigger">X</div>
+</div>`);
+    }
+
+    openModal(){
+        this.toggleSelector(this.classForShow)
+        this.toggleSelector(this.classForHide)
+    }
     beginEvents(){
         console.log('modalTrigger added')
-        document.querySelectorAll( this.selectorTrigger ).forEach( (el) => {
-            el.addEventListener('click', (ev) => {
-                this.toggleSelector(this.classForShow)
-                this.toggleSelector(this.classForHide)
-                ev.preventDefault()
-            })
-        })
-
         document.addEventListener('keyup',e => this.keyPressHandler(e))
-
+        document.getElementById('modalClose').addEventListener('click',ev => {
+            ev.preventDefault()
+            this.openModal()
+        })
     }
 
     keyPressHandler(e){
         if( this.escapeKeys.includes( e.keyCode) ){//when pressing escape key
-            this.toggleSelector(this.classForShow)
-            this.toggleSelector(this.classForHide)
+            this.openModal()
         }
     }
     toggleSelector(classToToggle) {
