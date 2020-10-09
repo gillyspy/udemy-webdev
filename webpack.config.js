be3f642c2f7fb4ea51dff6396abca5756b91d997
northg <gillyspy@gmail.com>
2020-10-08 21:09:05 -0400

    build process work continued

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
-const currentTask = process.env.npm_lifecycle_event;
-const path = require('path');
+const currentTask = process.env.npm_lifecycle_event
+const path = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const postcssplugins = [
     require('postcss-import'),
@@ -10,8 +12,25 @@ const postcssplugins = [
     require('autoprefixer')
 ];
 
+let cssConfig = {
+    test : /[.]css$/i,
+    use : [
+        //'style-loader',
+        'css-loader?url=false',
+        {
+            loader: 'postcss-loader',
+            options : {
+                plugins : postcssplugins
+            }
+        }
+    ]
+}
+
 let config = {
-    entry: './app/assets/scripts/App.js'
+    entry: './app/assets/scripts/App.js',
+    module : {
+        rules : [ cssConfig ]
+    }
 }
 
 if (currentTask == 'dev') {
@@ -28,49 +47,33 @@ if (currentTask == 'dev') {
         hot : true,
         port : 3000,
         host : '0.0.0.0'
-    };
-    config.mode = 'development'
-    config.module = {
-        rules : [
-            {
-                test : /[.]css$/i,
-                use : [
-                    'style-loader',
-                    'css-loader?url=false',
-                    {
-                        loader: 'postcss-loader',
-                        options : {
-                            plugins : postcssplugins
-                        }
-                    }
-                ]
-            }
-        ]
     }
-
+    config.mode = 'development'
+    cssConfig.use.unshift('style-loader')
 
 } else if (currentTask == 'build'){
     config.output = {
-        filename: 'bundled.js',
+        filename: '[name].[chunkhash].js',
+        chunkFilename : '[name].[chunkhash].js',
         path    : path.resolve(__dirname, 'dist') //multiplatform friendly
     };
     config.mode = 'production';
-    config.module = {
-        rules : [
-            {
-                test : /[.]css$/i,
-                use : [
-                    'style-loader',
-                    'css-loader?url=false',
-                    {
-                        loader: 'postcss-loader',
-                        options : {
-                            plugins : postcssplugins
-                        }
-                    }
-                ]
-            }
-        ]
+    config.optimization = {
+        splitChunks : {
+            chunks : 'all'
+        }
     }
+    config.plugins = [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'styles.[chunkhash].css'
+        })
+    ]
+    config.module = {
+        rules : [ cssConfig ]
+    }
+
+    cssConfig.use.unshift(MiniCssExtractPlugin.loader)
+    postcssplugins.push( require('cssnano') )
 }
 module.exports = config;
